a4658e3c82afa1119d80ed6bb4170e7160cc379e
Brunilda Muco <brunildamuco6@gmail.com>
2022-09-01 09:08:54 +0200

    added image first name and last name

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 type Quote = {
-  id: number
-  quote: string
-  author: string
-}
+  id: number;
+  quote: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  image: string;
+};
+
 
 function App() {
   const [quotes, setQuotes] = useState<Quote[]>([]);
@@ -13,7 +17,7 @@ function App() {
   useEffect(() => {
     fetch("http://localhost:4000/quotes")
       .then((resp) => resp.json())
-      .then((quotesFromServer) => setQuotes(quotesFromServer));
+      .then(quotesFromServer => setQuotes(quotesFromServer));
   });
 
   return (
@@ -21,9 +25,12 @@ function App() {
       <h2>Quotes</h2>
       <ul>
         {quotes.map((quote) => (
-          <div>
-            <li>{quote.quote}</li>
-            <li>{quote.author}</li>
+          <div className="quote">
+            <li className="list-items">{quote.quote}</li>
+            <li className="list-items">{quote.firstName}</li>
+            <li className="list-items">{quote.lastName}</li>
+            <li className="list-items">{quote.age} </li>
+            <img src={quote.image} alt={quote.firstName} width={50} />
           </div>
         ))}
       </ul>
